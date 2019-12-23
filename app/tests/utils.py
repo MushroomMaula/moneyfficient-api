ba2bad155b00d54608c1b7edb353232c63e1f7ae
MushroomMaula <maxrd79@gmail.com>
2019-12-23 14:23:59 +0100

    Add new fixtures

--- app/tests/utils.py
+++ app/tests/utils.py
@@ -1,11 +1,16 @@
 from faker import Faker
-from faker.providers import python, internet
+from faker.providers import python, internet, date_time
 
 
 fake = Faker()
 fake.add_provider(python)
 fake.add_provider(internet)
+fake.add_provider(date_time)
 
 
 def random_string():
     return fake.pystr()
+
+
+def random_float():
+    return fake.pyfloat(positive=True)
