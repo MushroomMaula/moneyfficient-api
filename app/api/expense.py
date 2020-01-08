5d52b7808e7eb97a58f999c488d48568d4527059
MushroomMaula <maxrd79@gmail.com>
2020-01-08 22:35:22 +0100

    Update routes to use new belongs_to_user function

--- app/api/expense.py
+++ app/api/expense.py
@@ -2,12 +2,10 @@ from fastapi import APIRouter, Depends, HTTPException
 
 from app import schemas
 from app import crud
-from app.core.security import manager
+from app.core.security import manager, belongs_to_user
 from app.db.base import get_db
 
 router = APIRouter()
-
-ACCESS_DENIED_ERROR = HTTPException(401, detail="This entry does not belong to the logged in user")
 
 
 @router.post("/new", response_model=schemas.Expense, status_code=201)
@@ -24,22 +22,19 @@ def get_all(user=Depends(manager), db=Depends(get_db)):
 @router.get('/{expense_id}', response_model=schemas.Expense, status_code=200)
 def get(expense_id: int, user=Depends(manager), db=Depends(get_db)):
     expense = crud.expense.get(expense_id, db)
-    if expense.owner_id != user.id:
-        raise ACCESS_DENIED_ERROR
+    belongs_to_user(expense, user)
     return expense
 
 
 @router.put('/{expense_id}', status_code=204)
 def edit(expense_id: int, data: schemas.ExpenseUpdate, user=Depends(manager), db=Depends(get_db)):
     expense = crud.expense.get(expense_id, db)
-    if expense.owner_id != user.id:
-        raise ACCESS_DENIED_ERROR
+    belongs_to_user(expense, user)
     crud.expense.update(expense.id, data, db)
 
 
 @router.delete('/{expense_id}', status_code=204)
 def delete(expense_id: int, user=Depends(manager), db=Depends(get_db)):
     expense = crud.expense.get(expense_id, db)
-    if expense.owner_id != user.id:
-        raise ACCESS_DENIED_ERROR
+    belongs_to_user(expense, user)
     crud.expense.delete(expense, db)
