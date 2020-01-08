fc8853f43c97c0fcc1651f9e2c847a7449f4a21d
MushroomMaula <maxrd79@gmail.com>
2020-01-08 22:34:58 +0100

    Add belongs_to_user function
    ? Maybe move this to the expense object

--- app/core/security.py
+++ app/core/security.py
@@ -1,6 +1,8 @@
 from fastapi_login import LoginManager
 from app.core import Config
-
+from app.core.errors import ACCESS_DENIED_ERROR
+from app.db_models.expense import Expense
+from app.db_models.user import User
 
 manager = LoginManager(Config.secret, tokenUrl='/auth/login')
 
@@ -11,3 +13,10 @@ def hash_password(plain_password):
 
 def verify_password(plain_password, hashed_password):
     return manager.pwd_context.verify(plain_password, hashed_password)
+
+
+def belongs_to_user(expense: Expense, user: User):
+    if expense.owner_id == user.id:
+        return True
+    else:
+        raise ACCESS_DENIED_ERROR
