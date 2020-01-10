1f3f90ef3218c9d2451a58eeabef06eca28579f8
MushroomMaula <maxrd79@gmail.com>
2020-01-10 23:10:08 +0100

    Add not authenticated error

--- app/core/errors.py
+++ app/core/errors.py
@@ -1,3 +1,7 @@
 from fastapi import HTTPException
 
 ACCESS_DENIED_ERROR = HTTPException(401, detail="This entry does not belong to the logged in user")
+
+
+class NotAuthenticatedError(Exception):
+    pass
