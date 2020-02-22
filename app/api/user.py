c01354ce7a83188f321c3f0195741e79fcc8673d
MushroomMaula <maxrd79@gmail.com>
2020-02-22 17:31:59 +0100

    Add token to returned payload

--- app/api/user.py
+++ app/api/user.py
@@ -1,17 +1,25 @@
 from fastapi import APIRouter, Depends, HTTPException
 
+from app.core.security import manager
 from app.db.base import get_db
-from app.schemas.user import UserCreate
 from app import crud
 from app import schemas
+from app.schemas import Token
 
 router = APIRouter()
 
 
-@router.post('/register', response_model=schemas.User, status_code=201)
-def register(user: UserCreate, db=Depends(get_db)):
+@router.post('/register', response_model=schemas.UserRegistered, status_code=201)
+def register(user: schemas.UserCreate, db=Depends(get_db)):
     if crud.user.get(user.email, db):
         raise HTTPException(
             409, 'A User with this email already exists'
         )
-    return crud.user.create(db, user)
+    user = schemas.User.from_orm(crud.user.create(db, user))
+
+    # create token
+    access_token = manager.create_access_token(
+        data={'sub': user.email}
+    )
+    token = Token(access_token=access_token, token_type="Bearer")
+    return schemas.UserRegistered(**user.dict(), login_credentials=token)
